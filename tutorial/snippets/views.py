71eddc68a835cf09ce275c3977f32a70482ff95a
Vasja Volin <vasja.volin@gmail.com>
2016-11-06 10:53:55 -0500

    add user list and detail views

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,5 +1,7 @@
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from django.contrib.auth.models import User
+from snippets.serializers import SnippetSerializer, UserSerializer
+
 from rest_framework import generics
 
 class SnippetList(generics.ListCreateAPIView):
@@ -9,3 +11,11 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetreiveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
